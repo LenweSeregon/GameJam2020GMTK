b9cf6d71edf45550752bd4ca84ad152f64d414d2
Nicolas Serf <serf.nicolas@gmail.com>
2020-07-11 01:10:37 +0200

    Commits filling folders + fonts

---
+++ GMTKGameJam2020/Assets/Equilibrium/Scripts/Scores/UI/UIScoreEntity.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+
+namespace Equilibrium
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class UIScoreEntity : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI m_playerNameText;
+        [SerializeField] private TextMeshProUGUI m_playerCoinText;
+        [SerializeField] private TextMeshProUGUI m_playerEnemyKilledText;
+        [SerializeField] private TextMeshProUGUI m_playerTimeText;
+        [SerializeField] private TextMeshProUGUI m_playerGlobalScoreText;
+
+        public void Init(string name, string coin, string enemyKilled, string time, string globalScore)
+        {
+            m_playerNameText.SetText(name);
+        }
+    }
+}
